d8fbed6e5f9168422b54861aa5507a53a7c67a04
mack millar <mackmillar@gmail.com>
2020-12-12 13:42:31 +0000

    setup flask

---
+++ controllers/member_controller.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+from models.member import Member
+import repositories.member_repository as member_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+# INDEX
+@members_blueprint.route("/")
+def members():
+    members = member_repository.select_all()
+    return render_template("/index.html", members=members)
